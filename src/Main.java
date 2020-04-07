45bd1f7793dc05c661ffc6931f8ccf174dd53785
Ivo Gabriel <ignborges@hotmail.com>
2020-04-06 23:29:28 -0400

    calculadora

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("digite um numero: ");
+
+int numero1 = sc.nextInt();
+
+System.out.print("digite outro numero: ");
+
+int numero2 = sc.nextInt();
+
+System.out.print("selecione uma operação: (*, /, +, -) ");
+
+String operacao = sc.next();
+
+if(operacao.equals("+")) {
+	
+	System.out.println("o resultado de " + numero1 + " + " + numero2 + " é igual á: "+ (numero1 + numero2));
+}
+
+if(operacao.equals("-")) {
+	
+	System.out.println("o resultado de " + numero1 + " - " + numero2 + " é igual á: "+ (numero1 - numero2));
+}
+
+if(operacao.equals("/")) {
+	
+	System.out.println("o resultado de " + numero1 + " / " + numero2 + " é igual á: "+ (numero1 / numero2));
+}
+
+if(operacao.equals("*")) {
+
+System.out.println("o resultado de " + numero1 + " * " + numero2 + " é igual á: "+ (numero1 * numero2));
+}
+
+	
+else
+
+System.out.println("operação invalida");
+System.out.println("operação invalidaselecione uma operação: (*, /, +, -) ");
+
+operacao = sc.next();
+		
+		
+	}
+}
