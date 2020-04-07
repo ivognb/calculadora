d76b483449a1e172129ab1d7210141179268fab4
Ivo Gabriel <ignborges@hotmail.com>
2020-04-06 23:32:11 -0400

    sc.close();

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,46 @@ public class Main {
 		
 		System.out.print("digite um numero: ");
 
-int numero1 = sc.nextInt();
+	int numero1 = sc.nextInt();
 
-System.out.print("digite outro numero: ");
-
-int numero2 = sc.nextInt();
-
-System.out.print("selecione uma operação: (*, /, +, -) ");
-
-String operacao = sc.next();
-
-if(operacao.equals("+")) {
+		System.out.print("digite outro numero: ");
 	
-	System.out.println("o resultado de " + numero1 + " + " + numero2 + " é igual á: "+ (numero1 + numero2));
-}
-
-if(operacao.equals("-")) {
+	int numero2 = sc.nextInt();
 	
-	System.out.println("o resultado de " + numero1 + " - " + numero2 + " é igual á: "+ (numero1 - numero2));
-}
-
-if(operacao.equals("/")) {
+		System.out.print("selecione uma operação: (*, /, +, -) ");
 	
-	System.out.println("o resultado de " + numero1 + " / " + numero2 + " é igual á: "+ (numero1 / numero2));
-}
-
-if(operacao.equals("*")) {
-
-System.out.println("o resultado de " + numero1 + " * " + numero2 + " é igual á: "+ (numero1 * numero2));
-}
-
+			String operacao = sc.next();
 	
-else
-
-System.out.println("operação invalida");
-System.out.println("operação invalidaselecione uma operação: (*, /, +, -) ");
-
-operacao = sc.next();
+	if(operacao.equals("+")) {
 		
-		
+		System.out.println("o resultado de " + numero1 + " + " + numero2 + " é igual á: "+ (numero1 + numero2));
 	}
-}
+	
+	if(operacao.equals("-")) {
+		
+		System.out.println("o resultado de " + numero1 + " - " + numero2 + " é igual á: "+ (numero1 - numero2));
+	}
+	
+	if(operacao.equals("/")) {
+		
+		System.out.println("o resultado de " + numero1 + " / " + numero2 + " é igual á: "+ (numero1 / numero2));
+	}
+	
+	if(operacao.equals("*")) {
+	
+		System.out.println("o resultado de " + numero1 + " * " + numero2 + " é igual á: "+ (numero1 * numero2));
+	}
+	
+		
+	else {
+	
+		System.out.println("operação invalida");
+	
+		System.out.println("selecione uma operação: (*, /, +, -) ");
+	
+	operacao = sc.next();
+			
+			}
+	sc.close();
+		}
+	}
